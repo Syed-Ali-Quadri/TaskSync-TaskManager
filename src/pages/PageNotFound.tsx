70bad520b1dcd2b871baf00ee5b2cfab99cd592a
Syed Ali Quadri <syedaliquadri100@gmail.com>
2025-04-30 18:19:43 +0530

    Add PageNotFound component and update routing to handle 404 errors

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router";
+import { FileQuestion } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+export default function NotFound() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen px-6 py-24 bg-background">
+			<div className="flex flex-col items-center max-w-md text-center">
+				<div className="flex items-center justify-center w-20 h-20 rounded-full bg-muted mb-8">
+					<FileQuestion className="h-10 w-10 text-muted-foreground" />
+				</div>
+
+				<h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-3">
+					404
+				</h1>
+				<h2 className="text-2xl font-semibold mb-3">Page not found</h2>
+
+				<p className="mb-8 text-muted-foreground">
+					Sorry, we couldn't find the page you're looking for. The
+					page might have been moved, deleted, or perhaps the URL was
+					mistyped.
+				</p>
+
+				<div className="flex flex-col sm:flex-row gap-4">
+					<Button asChild>
+						<Link to="/">Return to home</Link>
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,23 @@
-import Home from "@/pages/Home"
-import SignIn from "@/pages/SignIn"
-import SignUp from "@/pages/SignUp"
-import { Route, Routes } from "react-router"
+import { Route, Routes } from "react-router";
+import Home from "@/pages/Home";
+import SignIn from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
+import CreateTask from "@/pages/CreateTask";
+import EditTask from "@/pages/EditTask";
+import PageNotFound from "@/pages/PageNotFound";
 
 const AppRoutes = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="*" element={<PageNotFound />} />
+			<Route path="/signin" element={<SignIn />} />
+			<Route path="/signup" element={<SignUp />} />
+			<Route path="/signup" element={<SignUp />} />
+			<Route path="/create-task" element={<CreateTask />} />
+			<Route path="/edit-task/:taskId" element={<EditTask />} />
+		</Routes>
+	);
+};
 
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
-  )
-}
-
-export default AppRoutes
+export default AppRoutes;
